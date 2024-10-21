7c61e9c11513a731d44bb9af867c65dd147e8884
Thomas Jonstrup <thomas_jonstrup@hotmail.com>
2024-10-21 19:19:32 +0200

    Update +layout.svelte
    Added title for page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,10 @@
 	import logo from '$lib/assets/logo.svg';
 </script>
 
+<svelte:head>
+	<title>{'Translated'}</title>
+</svelte:head>
+
 <main class="container max-w-4xl mx-auto px-4 flex justify-center text-center flex-col text-white">
 	<header class="pt-[4rem]">
 		<img src={logo} alt="App logo" class="mx-auto" />
